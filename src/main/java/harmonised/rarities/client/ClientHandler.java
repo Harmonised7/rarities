66b3fd60d58063699d505851aba61873712c2e46
Harmonised <harmonised7@gmail.com>
2021-06-13 23:20:52 +0100

    Lowered Priority Tooltip

--- src/main/java/harmonised/rarities/client/ClientHandler.java
+++ src/main/java/harmonised/rarities/client/ClientHandler.java
@@ -3,19 +3,21 @@ package harmonised.rarities.client;
 import harmonised.rarities.config.Rarities;
 import harmonised.rarities.config.Rarity;
 import harmonised.rarities.util.Reference;
+import javafx.scene.layout.Priority;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.Color;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber( value = Dist.CLIENT, modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE )
 public class ClientHandler
 {
-    @SubscribeEvent
+    @SubscribeEvent( priority = EventPriority.LOWEST )
     public static void renderTooltip( ItemTooltipEvent event )
     {
         Rarity rarity = Rarities.getRarity( event.getItemStack() );
